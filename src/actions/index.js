88ee7aab9c055669456f7994361653f139b2433f
Jonni Laaksontammi <jonni.tammisto@skhole.fi>
2020-01-08 15:53:41 +0200

    NODE_PATH .env -> .jsconfig

--- src/actions/index.js
+++ src/actions/index.js
@@ -43,7 +43,7 @@ export const fetchConversations = (nextPage, callback) => async (
   getState
 ) => {
   const response = await axios.get(
-    `https://limitless-crag-95623.herokuapp.com/conversations?type=admin&admin_id=1881793&order=created_at&sort=desc&display_as=plaintext&per_page=50&page=${nextPage}`,
+    `http://localhost:3000/conversations?type=admin&admin_id=1881793&order=created_at&sort=desc&display_as=plaintext&per_page=50&page=${nextPage}`,
     intercomHeaders(getState().ui.intercomKey)
   );
   dispatch({
@@ -55,7 +55,7 @@ export const fetchConversations = (nextPage, callback) => async (
 
 export const fetchUser = userId => async (dispatch, getState) => {
   const response = await axios.get(
-    `https://limitless-crag-95623.herokuapp.com/users/${userId}`,
+    `http://localhost:3000/users/${userId}`,
     intercomHeaders(getState().ui.intercomKey)
   );
   dispatch({
